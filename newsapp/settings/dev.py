aaeeca0a4d56516b27c6af61f0a47c749b38502f
Abush Natanem <abushnatanem@gmail.com>
2025-08-12 15:04:29 +0300

    initial setup

---
+++ newsapp/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-v6)*3mmh9#^z4l2vzfa@d^xhqbu09b4tkmz*^h(sr09+2#6#j!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
